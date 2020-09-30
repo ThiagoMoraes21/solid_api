49e242a7d22b91bc3026d3b201f82b83192b0bd0
thiagoMoraes21 <thiago4x70@gmail.com>
2020-09-30 00:47:47 -0300

    finished users creation logic

---
+++ src/providers/implementations/MailtrapMailProvider.ts
@@ -0,0 +1,33 @@
+import { IMailProvider, IMessage } from '../IMailProvider';
+import nodemailer from 'nodemailer';
+import Mail from 'nodemailer/lib/mailer';
+
+export class MailtrapMailProvider implements IMailProvider {
+    private transporter: Mail;
+    
+    constructor() {
+        this.transporter = nodemailer.createTransport({
+            host: 'smtp.mailtrap.io',
+            port: 2525,
+            auth: {
+                user: 'f3c783b8ece11f',
+                pass: 'be4ef3346d9a14'
+            }
+        });
+    }
+
+    async sendMail(message: IMessage): Promise<void> {
+        await this.transporter.sendMail({
+            to: {
+                name: message.to.name,
+                address: message.to.email
+            },
+            from: {
+                name: message.from.name,
+                address: message.from.email
+            },
+            subject: message.subject,
+            html: message.body
+        });
+    }
+}
